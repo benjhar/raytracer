17109c512ae169d834480e460ee2ddb066fa2359
Ben Harris <benharris2514@gmail.com>
2024-12-07 16:13:47 +0000

    BVH

---
+++ src/aabb.rs
@@ -0,0 +1,93 @@
+use linalg::Point;
+
+use crate::{Interval, Ray};
+
+#[derive(Default, Clone, Copy)]
+pub struct AABB {
+    pub x: Interval,
+    pub y: Interval,
+    pub z: Interval,
+}
+
+impl AABB {
+    pub fn new(x: Interval, y: Interval, z: Interval) -> Self {
+        Self { x, y, z }
+    }
+
+    /// Treat the two points a and b as extrema for the bounding box, so we don't require a
+    /// particular minimum/maximum coordinate order
+    pub fn build(a: Point<f64, 3>, b: Point<f64, 3>) -> Self {
+        let x = if a.x() <= b.x() {
+            Interval::new(a.x(), b.x())
+        } else {
+            Interval::new(b.x(), a.x())
+        };
+
+        let y = if a.y() <= b.y() {
+            Interval::new(a.y(), b.y())
+        } else {
+            Interval::new(b.y(), a.y())
+        };
+
+        let z = if a.z() <= b.z() {
+            Interval::new(a.z(), b.z())
+        } else {
+            Interval::new(b.z(), a.z())
+        };
+
+        Self { x, y, z }
+    }
+
+    pub fn enclosing(box0: AABB, box1: AABB) -> Self {
+        let x = Interval::enclosing(box0.x, box1.x);
+        let y = Interval::enclosing(box0.y, box1.y);
+        let z = Interval::enclosing(box0.z, box1.z);
+
+        Self { x, y, z }
+    }
+
+    pub fn axis_interval(&self, n: usize) -> &Interval {
+        if n == 1 {
+            return &self.y;
+        }
+        if n == 2 {
+            return &self.z;
+        }
+        &self.x
+    }
+
+    pub fn hit(&self, r: &Ray, mut ray_t: Interval) -> bool {
+        let ray_orig = r.origin();
+        let ray_dir = r.direction();
+
+        for axis in 0..3 {
+            let ax = self.axis_interval(axis);
+            let adinv = 1. / ray_dir[axis];
+
+            let t0 = (ax.min - ray_orig[axis]) * adinv;
+            let t1 = (ax.max - ray_orig[axis]) * adinv;
+
+            if t0 < t1 {
+                if t0 > ray_t.min {
+                    ray_t.min = t0;
+                }
+                if t1 < ray_t.max {
+                    ray_t.max = t1;
+                }
+            } else {
+                if t1 > ray_t.min {
+                    ray_t.min = t1;
+                }
+                if t0 < ray_t.max {
+                    ray_t.max = t0;
+                }
+            }
+
+            if ray_t.max <= ray_t.min {
+                return false;
+            }
+        }
+
+        true
+    }
+}
---
+++ src/bvh.rs
@@ -0,0 +1,143 @@
+use std::{cmp::Ordering, sync::Arc};
+
+use linalg::vector::Vector;
+use rand::Rng;
+
+use crate::{
+    aabb::AABB, hittable::Hittable, hittable_list::HittableList, lambertian, material::Material,
+    sphere::Sphere,
+};
+
+#[derive(Clone)]
+pub struct BVHNode {
+    left: Arc<dyn Hittable>,
+    right: Arc<dyn Hittable>,
+    bbox: AABB,
+}
+
+impl BVHNode {
+    fn box_compare<O: Clone + Default + Hittable>(
+        a: &Arc<O>,
+        b: &Arc<O>,
+        axis_index: usize,
+    ) -> Ordering {
+        let a_bb = a.bounding_box();
+        let a_axis_interval = a_bb.axis_interval(axis_index);
+        let b_bb = b.bounding_box();
+        let b_axis_interval = b_bb.axis_interval(axis_index);
+        a_axis_interval
+            .min
+            .partial_cmp(&b_axis_interval.min)
+            .unwrap()
+    }
+
+    fn box_x_compare<O: Clone + Default + Hittable>(a: &Arc<O>, b: &Arc<O>) -> Ordering {
+        Self::box_compare(a, b, 0)
+    }
+    fn box_y_compare<O: Clone + Default + Hittable>(a: &Arc<O>, b: &Arc<O>) -> Ordering {
+        Self::box_compare(a, b, 1)
+    }
+    fn box_z_compare<O: Clone + Default + Hittable>(a: &Arc<O>, b: &Arc<O>) -> Ordering {
+        Self::box_compare(a, b, 2)
+    }
+
+    pub fn new<O: Clone + Default + Hittable + 'static>(
+        mut objects: Vec<Arc<O>>,
+        start: usize,
+        end: usize,
+    ) -> Self {
+        let mut rng = rand::thread_rng();
+        let axis = rng.gen_range(0..=2);
+
+        let comparator = match axis {
+            0 => Self::box_x_compare,
+            1 => Self::box_y_compare,
+            _ => Self::box_z_compare,
+        };
+
+        let object_span = end - start;
+
+        let (left, right) = if object_span == 1 {
+            (
+                objects[start].clone() as Arc<dyn Hittable>,
+                objects[start].clone() as Arc<dyn Hittable>,
+            )
+        } else if object_span == 2 {
+            (
+                objects[start].clone() as Arc<dyn Hittable>,
+                objects[start + 1].clone() as Arc<dyn Hittable>,
+            )
+        } else {
+            objects.sort_by(comparator);
+
+            let mid = start + object_span / 2;
+            (
+                Arc::new(BVHNode::new(objects.clone(), start, mid)) as Arc<dyn Hittable>,
+                Arc::new(BVHNode::new(objects.clone(), mid, end)) as Arc<dyn Hittable>,
+            )
+        };
+
+        let bbox = AABB::enclosing(left.bounding_box(), right.bounding_box());
+
+        Self { left, right, bbox }
+    }
+
+    pub fn build<O: Clone + Default + Hittable + 'static>(list: HittableList<O>) -> Self {
+        let len = list.objects.len();
+        Self::new(list.objects, 0, len)
+    }
+}
+
+impl Hittable for BVHNode {
+    fn hit(
+        &self,
+        ray: &crate::Ray,
+        ray_t: crate::Interval,
+        record: &mut crate::hittable::HitRecord,
+    ) -> bool {
+        if !self.bbox.hit(ray, ray_t) {
+            return false;
+        }
+
+        let hit_left = self.left.hit(ray, ray_t, record);
+        let hit_right = self.right.hit(ray, ray_t, record);
+
+        hit_left || hit_right
+    }
+
+    fn bounding_box(&self) -> AABB {
+        self.bbox
+    }
+}
+
+impl Default for BVHNode {
+    fn default() -> Self {
+        Self {
+            left: Arc::new(Sphere::new(
+                Vector::zero(),
+                None,
+                0.0,
+                Arc::new(lambertian::Lambertian::default()),
+            )),
+            right: Arc::new(Sphere::new(
+                Vector::zero(),
+                None,
+                0.0,
+                Arc::new(lambertian::Lambertian::default()),
+            )),
+            bbox: AABB::default(),
+        }
+    }
+}
+
+// impl Material for BVHNode {
+//     fn scatter(
+//             &self,
+//             ray_in: &crate::Ray,
+//             record: &crate::hittable::HitRecord,
+//             attenuation: &mut crate::colour::Colour,
+//             scattered: &mut crate::Ray,
+//         ) -> bool {
+//         if self.left.hit(ray_in, ray_t, record)
+//     }
+// }
