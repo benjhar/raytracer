9e24cafb26ea5d34e4a478c333bb29a129f9ffb0
Ben Harris <benharris2514@gmail.com>
2024-12-10 12:05:49 +0000

    Perlin noise

--- src/noise_texture.rs
+++ src/noise_texture.rs
@@ -2,30 +2,33 @@ use crate::{colour::Colour, perlin::Perlin, texture::Texture};
 
 pub struct NoiseTexture {
     noise: Perlin,
+    scale: f64,
 }
 
 impl NoiseTexture {
-    pub fn new() -> Self {
+    pub fn new(scale: f64) -> Self {
         Self {
             noise: Perlin::new(None),
+            scale,
         }
     }
 
-    pub fn with_seed(seed: u64) -> Self {
+    pub fn with_seed(seed: u64, scale: f64) -> Self {
         Self {
             noise: Perlin::new(Some(seed)),
+            scale,
         }
     }
 }
 
 impl Texture for NoiseTexture {
     fn value(&self, _: f64, _: f64, p: &linalg::Point<f64, 3>) -> crate::colour::Colour {
-        Colour::new([1.; 3]) * self.noise.noise(*p)
+        Colour::new([1.; 3]) * 0.5 * (1. + self.noise.noise(self.scale * *p))
     }
 }
 
 impl Default for NoiseTexture {
     fn default() -> Self {
-        Self::new()
+        Self::new(1.)
     }
 }
--- src/perlin.rs
+++ src/perlin.rs
@@ -1,12 +1,12 @@
 use std::array;
 
-use linalg::Point;
+use linalg::{vector::Vector, Point};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 const POINT_COUNT: usize = 256;
 
 pub struct Perlin {
-    randfloat: [f64; POINT_COUNT],
+    randvec: [Vector<f64, 3>; POINT_COUNT],
     perm_x: [u32; POINT_COUNT],
     perm_y: [u32; POINT_COUNT],
     perm_z: [u32; POINT_COUNT],
@@ -19,14 +19,14 @@ impl Perlin {
         } else {
             StdRng::from_entropy()
         };
-        let randfloat: [f64; POINT_COUNT] = array::from_fn(|_| rng.gen());
+        let randvec = array::from_fn(|_| Vector::random_range(-1., 1.).unit());
 
         let perm_x = Self::permute(array::from_fn(|i| i as u32), &mut rng);
         let perm_y = Self::permute(array::from_fn(|i| i as u32), &mut rng);
         let perm_z = Self::permute(array::from_fn(|i| i as u32), &mut rng);
 
         Self {
-            randfloat,
+            randvec,
             perm_x,
             perm_y,
             perm_z,
@@ -34,10 +34,25 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point<f64, 3>) -> f64 {
-        let a: [f64; 3] = (4. * p).into();
-        let [i, j, k] = a.map(|x| x.abs() as usize & 255);
+        let p_floor = p.map(f64::floor);
+        let offset_vector = p - p_floor;
+        let [i, j, k] = p_floor.map(|a| a as i64).to_array();
 
-        self.randfloat[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
+        let mut c = [[[Vector::zero(); 2]; 2]; 2];
+
+        for di in 0..2i64 {
+            for dj in 0..2i64 {
+                for dk in 0..2i64 {
+                    c[di as usize][dj as usize][dk as usize] = self.randvec[(self.perm_x
+                        [(i + di) as usize & 255]
+                        ^ self.perm_y[(j + dj) as usize & 255]
+                        ^ self.perm_z[(k + dk) as usize & 255])
+                        as usize];
+                }
+            }
+        }
+
+        Self::perlin_interp(c, offset_vector)
     }
 
     fn permute(mut p: [u32; POINT_COUNT], rng: &mut StdRng) -> [u32; POINT_COUNT] {
@@ -47,5 +62,28 @@ impl Perlin {
         }
 
         p
+    }
+
+    fn perlin_interp(c: [[[Vector<f64, 3>; 2]; 2]; 2], offset_vector: Vector<f64, 3>) -> f64 {
+        let hermite_offset = Self::hermite_cubic(offset_vector);
+        let one = Vector::one();
+        let mut acc = 0.;
+        for (i, plane) in c.iter().enumerate() {
+            for (j, row) in plane.iter().enumerate() {
+                for (k, cell) in row.iter().enumerate() {
+                    let ijk = Vector::new([i as f64, j as f64, k as f64]);
+                    let weight_v = offset_vector - ijk;
+                    acc += ((ijk * hermite_offset) + (one - ijk) * (one - hermite_offset))
+                        .product()
+                        * cell.dot(&weight_v);
+                }
+            }
+        }
+
+        acc
+    }
+
+    fn hermite_cubic(v: Vector<f64, 3>) -> Vector<f64, 3> {
+        v * v * (Vector::new([3.; 3]) - 2. * v)
     }
 }
